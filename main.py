1c0af8f2d12ad74ebc1ce148e3b1c714ad21113d
rpidanny <abhishekmaharjan1993@gmail.com>
2019-08-01 11:38:30 +0200

    Add get_topics and get_content

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from tutorialsdojo import TutorialsDojo
 
 def main():
     td = TutorialsDojo()
-    for course in td.get_courses():
-        print(course)
+    # for group in td.get_groups():
+    #     print(' [+] {}'.format(group['title']))
+    #     for topic in td.get_topics(group):
+    #         print('    - {} ({})'.format(topic['title'], topic['url']))
+    #     print('\n')
 
+    print(td.get_content({'url': 'https://tutorialsdojo.com/aws-billing-and-cost-management/'}))
 
 if __name__ == "__main__":
     main()
--- tutorialsdojo.py
+++ tutorialsdojo.py
@@ -12,16 +12,39 @@ class TutorialsDojo():
         plain_text = source_code.text
         return BeautifulSoup(plain_text, "html.parser")
     
-    def get_courses(self):
+    def get_groups(self):
         page = self.fetch_page(self.base_url)
         divs = page.findAll('div', {'class': 'fusion-button-wrapper fusion-aligncenter'})
-        courses = []
+        groups = []
 
         for div in divs:
             link = div.find('a')
             title = div.find('span', {'class': 'fusion-button-text fusion-button-text-left'})
-            courses.append({
+            groups.append({
                 'title': title.text,
                 'url': link['href']
             })
-        return courses
+        return groups
+    
+    def get_topics(self, group):
+        page = self.fetch_page(group['url'])
+        divs = page.findAll('div', {'class': 'fusion-button-wrapper fusion-aligncenter'})
+        topics = []
+
+        for div in divs:
+            link = div.find('a')
+            title = div.find('span', {'class': 'fusion-button-text fusion-button-text-left'})
+            topics.append({
+                'title': title.text,
+                'url': link['href']
+            })
+        return topics
+
+    def get_content(self, topic):
+        page = self.fetch_page(topic['url'])
+        div = page.find('div', {'class': 'fusion-text'})
+        
+        for crap in div.findAll('p', {'data-pm-slice': '1 1 []'}):
+            crap.decompose()
+
+        return div.text
